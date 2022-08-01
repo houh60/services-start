60d72dc50c5312bd796abefa774e162bc9601d09
William Hou <williamhou38@yahoo.com>
2022-07-31 21:36:23 -0500

    new change

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -2,6 +2,6 @@
 export class LoggingService {
 
   logStatusChange(status: string) {
-    console.log('A server status changed, new status: ' + accountStatus);
+    console.log('A server status changed, new status: ' + status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
@@ -6,13 +7,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(
+    private accountService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
   }
 }
